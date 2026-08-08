96d6ca938400fb02b14124aae0530576146f4c5a
Komal <komalsingh54@gmail.com>
2026-08-08 14:41:36 +0100

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Komal Singh on 08/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
